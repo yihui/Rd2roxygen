ccdd70f5dba0bc18ae78aac3353c0079548eaed1
Yihui Xie <xie@yihui.name>
2010-12-11 00:52:18 -0600

    package description Rd

---
+++ R/Rd2roxygen-package.R
@@ -0,0 +1,17 @@
+##' Convert Rd to roxygen documentation and utilities to enhance roxygen.
+##' This package contains functions to convert Rd to roxygen documentation.
+##' It can parse an Rd file to a list (\code{\link{parse_file}}),
+##' create the roxygen documentation (\code{\link{create_roxygen}}) and
+##' update the original R script (e.g. the one containing the
+##' definition of the function) accordingly (\code{\link{Rd2roxygen}}).
+##' This package also provides utilities which can help developers
+##' build packages using roxygen more easily (\code{\link{rab}}).
+##'
+##' @name Rd2roxygen-package
+##' @aliases Rd2roxygen-package
+##' @docType package
+##' @author Hadley Wickham and Yihui Xie
+##' @importFrom tools parse_Rd
+##' @examples
+##' ## see the package vignette: vignette('Rd2roxygen')
+NULL
