207ee3ac5ad507d4af5837c7f8629565e726ed5b
Yihui Xie <xie@yihui.name>
2011-07-28 15:01:02 -0500

    remove \selectlanguage{} statements in LaTeX

--- inst/doc/clean-up.R
+++ inst/doc/clean-up.R
@@ -6,6 +6,7 @@ idx = grep('\\\\documentclass', x)
 if (idx > 1) x = x[-(1:(idx-1))]
 idx = grep('\\\\bibliography\\{', x)
 x[idx] = sub('\\{.*Rd2roxygen_inst_doc_', '{', x[idx])
+x = gsub('%\\\\selectlanguage\\{english\\}[%]*', '', x)
 writeLines(x, 'Rd2roxygen.tex')
 file.rename('Rd2roxygen.tex', 'Rd2roxygen.Rnw')
 unlink(sprintf('Rd2roxygen.%s', c('aux', 'log', 'map', 'out', 'pdf')))
