3408242174ac6830b324405f50fdc19f1b9d706b
Yihui Xie <xie@yihui.name>
2011-08-15 16:41:36 -0500

    should not search for data and package names in R scripts

--- R/rd2roxygen.R
+++ R/rd2roxygen.R
@@ -192,7 +192,9 @@ Rd2roxygen <- function(pkg, nomatch, usage = FALSE) {
         tryf <- unique(c(tryf[file.exists(file.path(R.dir, tryf))],
                          list.files(R.dir, '\\.[Rr]$')))
         idx <- integer(0)
-        message("looking for the object '", fname, "' in:")
+        ## should not search data/package names in R scripts
+        if (isTRUE(parsed$docType %in% c('data', 'package')))
+            tryf <- NULL else message("looking for the object '", fname, "' in:")
         for (i in tryf) {
             r <- file.path(R.dir, i)
             idx <- grep(sprintf('^[[:space:]]*(`|"|\'|)(%s)(`|"|\'|)[[:space:]]*(<-|=)',
