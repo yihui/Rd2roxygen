1f6ff22acc6bab9e4b48ba22bc91acc03e77e9a1
Yihui Xie <xie@yihui.name>
2011-11-20 16:30:53 -0600

    update the clean-up script; use tweakr to automatically generate bib

--- vignettes/clean-up.R
+++ vignettes/clean-up.R
@@ -1,13 +1,18 @@
 #!/usr/bin/env Rscript
 
+## automatic bib generation
+library(tweakr)
+write_citation(c('Rd2roxygen', 'roxygen2', 'formatR', 'knitr'), file = 'Rd2roxygen.bib')
+
 ## deal with LyX filename mangling
-x = readLines('Rd2roxygen.tex')
-idx = grep('\\\\documentclass', x)
-if (idx > 1) x = x[-(1:(idx-1))]
-idx = grep('\\\\bibliography\\{', x)
-x[idx] = sub('\\{.*Rd2roxygen_inst_doc_', '{', x[idx])
-x = gsub('%\\\\selectlanguage\\{english\\}[%]*', '', x)
-writeLines(x, 'Rd2roxygen.tex')
-file.rename('Rd2roxygen.tex', 'Rd2roxygen.Rnw')
-unlink(sprintf('Rd2roxygen.%s', c('aux', 'log', 'map', 'out', 'pdf')))
+if (file.exists('Rd2roxygen.tex')) {
+    x = readLines('Rd2roxygen.tex')
+    idx = grep('\\\\documentclass', x)
+    if (idx > 1) x = x[-(1:(idx-1))]
+    idx = grep('\\\\bibliography|\\\\includegraphics', x)
+    x[idx] = sub('\\{.*Rd2roxygen_vignettes_', '{', x[idx])
+    writeLines(x, 'Rd2roxygen.tex')
+    file.rename('Rd2roxygen.tex', 'Rd2roxygen.Rnw')
+}
+unlink(sprintf('Rd2roxygen.%s', c('aux', 'log', 'out', 'pdf')))
 ## now we can cheat Sweave :-)
