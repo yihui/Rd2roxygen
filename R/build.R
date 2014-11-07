52c822350c70812921ea3e62eccd91c3e3c06929
Yihui Xie <xie@yihui.name>
2014-11-07 16:09:25 -0600

    move the code formatting examples from reformat_code() to tidy_examples()

--- R/build.R
+++ R/build.R
@@ -94,38 +94,7 @@ reformat_code = function(path, ...) {
     idx1 = grep(tags, rd)
     if (length(idx1) && any(idx1 > idx0))
       idx1 = min(idx1[idx1 > idx0]) - 1 else idx1 = tail(grep('\\}$', rd), 1)
-    tmp = rd[idx0:idx1]
-    tmp[1] = sub('^\\\\examples\\{', '', tmp[1])
-    nn = length(tmp)
-    tmp[nn] = sub('\\}$', '', tmp[nn])
-    txt = gsub('\\%', '%', tmp, fixed = TRUE) # will escape % later
-    txt = sub('^\\\\+dont(run|test|show)', 'tag_name_dont\\1 <- function() ', txt)
-    txt = tidy_code(txt, ...)
-    if (!inherits(txt, 'try-error')) {
-      txt = gsub('(^|[^\\])%', '\\1\\\\%', txt)
-      txt = gsub('tag_name_dont(run|test|show) <- function\\(\\) \\{', '\\\\dont\\1{', txt)
-      txt[txt == ''] = '\n'
-      txt = unlist(strsplit(txt, '\n'))
-      # remove the four spaces introduced by disguising \\dontrun as a function
-      if (length(idx2 <- grep('\\\\dont(run|test|show)\\{', txt))) {
-        for (i in idx2) {
-          j = i + 1
-          while (txt[j] != '}') {
-            txt[j] = sub('^    ', '', txt[j])
-            j = j + 1
-          }
-        }
-      }
-      txt = paste(txt, rep(c('\n', ''), c(length(txt) - 1, 1)), sep = '', collapse = '')
-      txt[1] = paste('\\examples{', txt[1], sep = '')
-      nn0 = length(txt)
-      txt[nn0] = paste(txt[nn0], '}', sep = '')
-      rd[idx0] = paste(txt, collapse = '\n')
-      if (idx1 > idx0) rd = rd[-((idx0 + 1):idx1)]
-    } else {
-      message('(!) failed to reformat examples code in ', path)
-      message(paste('   ', tmp, collapse = '\n'))
-    }
+    rd = tidy_examples(rd, idx0, idx1, ...)
   }
   if (length(idx0 <- grep('^\\\\usage\\{', rd))) {
     idx1 = grep(tags, rd)
